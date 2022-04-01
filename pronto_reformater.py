3b9996ebe2b3648273608b88b40f279c93b4b62e
Neji128 <webbdesmondj@gmail.com>
2022-04-01 16:03:24 -0700

    S3 connection established. Still need to download files and reformat.

--- pronto_reformater.py
+++ pronto_reformater.py
@@ -1,10 +1,14 @@
 import boto3
+import json
+
+with open("./credentials.json", "r") as data:
+    credentials = json.load(data)
 
 s3 = boto3.resource(
-    service_name='s3',
-    region_name='',
-    aws_access_key_id='xxxx',
-    aws_secret_access_key='xxx'
+    service_name="s3", 
+    region_name=credentials["region_name"], 
+    aws_access_key_id=credentials["access_key_id"],
+    aws_secret_access_key=credentials["secret_access_key"]
 )
 
 for bucket in s3.buckets.all():
