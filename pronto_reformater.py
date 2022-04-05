6d0a5d0564dd7fc7772d5f4c32dfc0c72e8117aa
Neji128 <webbdesmondj@gmail.com>
2022-04-05 16:45:02 -0700

    s3 download flow generated. Still need to parse txt files and create upload flow.

--- pronto_reformater.py
+++ pronto_reformater.py
@@ -1,15 +1,33 @@
 import boto3
 import json
+import os
 
-with open("./credentials.json", "r") as data:
+#os.mkdir("./temp_files")
+
+with open("./AWS_Credentials.json", "r") as data:
     credentials = json.load(data)
 
-s3 = boto3.resource(
-    service_name="s3", 
-    region_name=credentials["region_name"], 
+#Creating AWS session
+session = boto3.Session(
     aws_access_key_id=credentials["access_key_id"],
     aws_secret_access_key=credentials["secret_access_key"]
 )
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+#Establishing session resource
+s3 = session.resource("s3")
+
+target_bucket = s3.Bucket("pronto-logs-ftp")
+
+#Establishing list of object names within bucket object
+object_keys = [object.key for object in target_bucket.objects.filter(Prefix="dez-pronto-log-dev")]
+
+object_file_names = [object[19:] for object in object_keys][1:]
+
+#removing blank value
+object_keys = object_keys[1:]
+
+#Downloading folder contents from bucket to temperary folder
+[target_bucket.download_file(object_keys[x], f"./temp_files/{object_file_names[x]}") for x in range(len(object_keys))]
+
+#Deleting temperary files
+#os.rmdir("temp_files")
